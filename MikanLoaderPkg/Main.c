6a644bd4892304e33160768bb91417ac25952644
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-10-13 18:49:18 +0900

    02b_chotto

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -8,3 +8,19 @@ EFI_STATUS EFIAPI UefiMain(
   while (1);
   return EFI_SUCCESS;
 }
+
+
+EFI_STATUS GetMemoryMap(struct Memory* map){
+  if(map->buffer == NULL){
+    return EFI_BUFFER_TOO_SMALL;
+  }
+  map-> map_size = map -> buffer_size;
+  return gBS->GetMemoryMap(
+    &map->map_size,
+    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+    &map->map_key,
+    &map->descriptor_size,
+    &map->descriptor_version
+  );
+}
+
